557a033342b4914482a60c8ce153e739e16f910c
Mark Hildebrand <mhildebrand@ucdavis.edu>
2020-03-20 10:43:29 -0700

    Initial Commit

--- src/ExperimentsDB.jl
+++ src/ExperimentsDB.jl
@@ -1,5 +1,156 @@
 module ExperimentsDB
 
-greet() = print("Hello World!")
+# stdlib
+using Serialization
+
+# deps
+using DataStructures
+using Tables
+using PrettyTables
+
+tokey(::Type{T}, x) where {K, T <: AbstractDict{K}} = K(x)
+
+todict(::Type{T}, x) where {T} = T(tokey(T, k) => v for (k,v) in pairs(x))
+todict(::Type{T}, x::T) where {T} = x
+todict(x) = todict(SortedDict{Symbol,Any}, x)
+
+# A row in the table
+struct Entry <: Tables.AbstractRow
+    parameters::SortedDict{Symbol,Any}
+    data::Dict{String,Any}
+
+    function Entry(parameters, data = Dict{String,Any}())
+        parameters = todict(parameters)
+        data = todict(Dict{String,Any}, data)
+        return new(parameters, data)
+    end
+end
+
+parameters(x::Entry) = getfield(x, :parameters)
+Base.:(==)(x::Entry, y::Entry) = parameters(x) == parameters(y)
+Base.in(x::SortedDict{Symbol,Any}, y::Entry) = todict(x) == parameters(y)
+
+# Use a Sentinel instead of `nothing` because ... well ... the entries could BE `nothing`.
+struct Sentinel end
+function Base.issubset(x::SortedDict{Symbol,Any}, y::Entry)
+    params = parameters(y)
+    for (k,v) in x
+        val = get(params, k, Sentinel())
+        val == v || return false
+    end
+    return true
+end
+Base.keys(x::Entry) = keys(parameters(x))
+
+Base.getindex(x::Entry) = getfield(x, :data)
+Base.getindex(x::Entry, nm::Symbol) = parameters(x)[nm]
+
+# Table API
+Tables.getcolumn(x::Entry, nm::Symbol) = x[nm]
+Tables.getcolumn(x::Entry, i::Int) = Tables.getcolumn(x, collect(keys(x))[i])
+Tables.columnnames(x::Entry) = collect(keys(x))
+
+# Adding empty fields to an Entry
+function expand!(d::SortedDict{Symbol,Any}, ks)
+    for k in ks
+        get!(d, k, nothing)
+    end
+    return nothing
+end
+expand!(E::Entry, ks) = expand!(parameters(E), ks)
+
+#####
+##### Database
+#####
+
+struct Database
+    entries::Vector{Entry}
+    keys::Vector{Symbol}
+end
+Database() = Database(Entry[], Symbol[])
+Base.keys(db::Database) = db.keys
+
+Tables.istable(::Type{Database}) = true
+Tables.rowaccess(::Type{Database}) = true
+Tables.rows(db::Database) = db.entries
+
+datamerge(a, b) = b
+function Base.setindex!(db::Database, data, parameters)
+    parameters = todict(parameters)
+    data = todict(Dict{String,Any}, data)
+
+    # Add in all of the keys from the db.
+    expand!(parameters, keys(db))
+    entry = Entry(parameters, data)
+
+    # If the keys of this new entry are a subset of the keys already existing in the db,
+    # then we have to check if this entry already exists in the Database.
+    if issubset(keys(parameters), keys(db))
+        i = findfirst(isequal(entry), Tables.rows(db))
+        if !isnothing(i)
+            merge!(datamerge, db[i][], data)
+            return nothing
+        end
+    else
+        # Expand the existing entries for any new keys that were potentially created.
+        expand!.(Tables.rows(db), Ref(keys(entry)))
+        append!(keys(db), setdiff(keys(entry), keys(db)))
+        sort!(keys(db))
+    end
+
+    # Now we add a new row.
+    push!(db.entries, entry)
+    return nothing
+end
+
+# Indexing methods for searching and reducing the size of the table.
+Base.length(x::Database) = length(x.entries)
+Base.isempty(x::Database) = isempty(x.entries)
+Base.iterate(x::Database) = iterate(x.entries)
+Base.iterate(x::Database, s) = iterate(x.entries, s)
+Base.eltype(x::Database) = eltyps(x.entries)
+
+# Base.in
+# We're essentially looking for `d` to have a subset of
+Base.in(d::AbstractDict, s::Database) = any(x -> in(todict(d), x), s.entries)
+Base.issubset(d::AbstractDict, s::Database) = any(x -> issubset(todict(d), x), s.entries)
+
+# `getindex` Overloading
+Base.getindex(x::Database, i::Integer) = x.entries[i]
+Base.getindex(x::Database, i) = Database(x.entries[i], x.keys)
+Base.getindex(x::Database, ::Nothing) = Database()
+function Base.getindex(x::Database, i::SortedDict{Symbol,Any})
+    return x[findall(x -> issubset(i, x), x.entries)]
+end
+Base.getindex(x::Database, i::NamedTuple) = x[todict(i)]
+Base.getindex(x::Database; kw...) = x[(;kw...,)]
+Base.getindex(x::Database, s::Symbol) = [ e[s] for e in x ]
+
+#####
+##### Tables Interface
+#####
+
+# Pretty Printing
+function Base.show(io::IO, db::Database)
+    if length(db) == 0
+        println("Empty Database")
+    else
+        PrettyTables.pretty_table(io, db, keys(db))
+    end
+end
+
+#####
+##### Saving and Loading
+#####
+
+load(path) = deserialize(path)::Database
+function save(path, db)
+    # Serialize to a temporary file and then use `mv`.
+    # This is just in case we get interrupted while serializing.
+    temp = tempname()
+    serialize(temp, db)
+    mv(temp, path; force = true)
+    return nothing
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,193 @@
 using ExperimentsDB
 using Test
 
-@testset "ExperimentsDB.jl" begin
-    # Write your own tests here.
+using Tables
+using DataStructures
+
+# For testing `datamerge`
+struct Merger
+    val::Int
 end
+ExperimentsDB.datamerge(a::Merger, b::Merger) = Merger(a.val + b.val)
+
+function keycheck(db::ExperimentsDB.Database)
+    ks = keys(db)
+    for entry in db
+        @test collect(keys(entry)) == ks
+    end
+end
+
+@testset "ExperimentsDB.jl" begin
+    ### todict
+    x = ExperimentsDB.todict((a = 1, b = 2))
+    @test isa(x, DataStructures.SortedDict{Symbol, Any})
+    @test length(x) == 2
+    @test x[:a] == 1
+    @test x[:b] == 2
+
+    # Test pass-through logic
+    @test ExperimentsDB.todict(x) === x
+
+    # Pass in a normal dict
+    y = Dict("a" => 1, "b" => 2)
+    x = ExperimentsDB.todict(y)
+    @test isa(x, DataStructures.SortedDict{Symbol, Any})
+    @test length(x) == 2
+    @test x[:a] == 1
+    @test x[:b] == 2
+
+    y = Dict(:a => 1, :b => 2)
+    x = ExperimentsDB.todict(y)
+    @test isa(x, DataStructures.SortedDict{Symbol, Any})
+    @test length(x) == 2
+    @test x[:a] == 1
+    @test x[:b] == 2
+
+    x = ExperimentsDB.todict(DataStructures.SortedDict{String,Any}, y)
+    @test isa(x, DataStructures.SortedDict{String, Any})
+    @test length(x) == 2
+    @test x["a"] == 1
+    @test x["b"] == 2
+
+    ### Entry
+    data = Dict(
+        "entry_1" => 1,
+        "entry_2" => 2,
+    )
+    A = ExperimentsDB.Entry((a = 1, b = 2), data)
+    @test collect(keys(A)) == [:a, :b]
+    @test A[] == data
+    @test A[:a] == 1
+    @test A[:b] == 2
+
+    # Tables API
+    @test Tables.columnnames(A) == [:a, :b]
+    @test Tables.getcolumn(A, :a) == 1
+    @test Tables.getcolumn(A,  1) == 1
+    @test Tables.getcolumn(A, :b) == 2
+    @test Tables.getcolumn(A,  2) == 2
+
+    params = ExperimentsDB.parameters(A)
+    @test length(params) == 2
+    @test params[:a] == 1
+    @test params[:b] == 2
+
+    # Test ==
+    B = ExperimentsDB.Entry(Dict("a" => 1, "b" => 2))
+    @test A == B
+
+    @test issubset(ExperimentsDB.todict((a = 1,)), A)
+    @test issubset(ExperimentsDB.todict((b = 2,)), A)
+    @test issubset(ExperimentsDB.todict((a = 1, b = 2,)), A)
+    @test !issubset(ExperimentsDB.todict((a = 2, b = 2,)), A)
+    @test !issubset(ExperimentsDB.todict((c = nothing,)), A)
+    @test in(ExperimentsDB.todict((a = 1, b = 2,)), A)
+    @test !in(ExperimentsDB.todict((a = 1,)), A)
+
+    # expand!
+    nt = (a = 1, b = 2)
+    x = ExperimentsDB.todict(nt)
+    ExperimentsDB.expand!(x, [:a, :c])
+    @test x[:a] == 1
+    @test x[:b] == 2
+    @test x[:c] == nothing
+
+    entry = ExperimentsDB.Entry(nt)
+    ExperimentsDB.expand!(entry, [:a, :c])
+    @test ExperimentsDB.parameters(entry) == x
+
+    #####
+    ##### DataBase
+    #####
+    db = ExperimentsDB.Database()
+    @test length(db) == 0
+    @test keys(db) == Symbol[]
+
+    # Show empty database
+    show(devnull, db)
+
+    data1 = Dict(
+        "merger" => Merger(10),
+        "int" => 10,
+    )
+    data2 = Dict(
+        "merger" => Merger(20),
+        "int" => 20,
+    )
+    data3 = Dict(
+        "merger" => Merger(30),
+        "int" => 30
+    )
+
+    db[(a = 1, c = 2)] = data1
+    @test length(db) == 1
+    @test keys(db) == [:a, :c]
+
+    ### We now have several different cases to test.
+    ###
+    ### Case 1: Keys of new entry are a subset of the existing keys, but a match is not found.
+    ### Case 2: Keys of new entry are a subset of the existing keys, but a match is found.
+    ### Case 3: Not a subset
+
+    ## Case 1
+    db[(a = 1,)] = data2
+    @test length(db) == 2
+    @test keys(db) == [:a, :c]
+
+    # The second entry should have had its keys expanded
+    entry = db[2]
+    @test collect(keys(entry)) == [:a, :c]
+    @test entry[:a] == 1
+    @test entry[:c] == nothing
+    @test db[1][] == data1
+    @test db[2][] == data2
+
+    ## Case 2
+    db[(a = 1, c = 2)] = data2
+    @test length(db) == 2
+    keycheck(db)
+
+    # Check if data was merged properly.
+    data = db[1][]
+    # The integer should have been replaced.
+    @test data["int"] == 20
+    # The Merger should have been merged
+    @test data["merger"] == Merger(10 + 20)
+
+    ## Case 3
+    db[(b = 3,)] = data3
+    @test length(db) == 3
+    @test keys(db) == [:a, :b, :c]
+    keycheck(db)
+
+    ### Done testing merging logic
+    # Test showing DB
+    show(devnull, db)
+
+    # Now, do some testing for "getindex"
+    db2 = db[[1,2,]]
+    @test isa(db2, ExperimentsDB.Database)
+    @test length(db2) == 2
+    @test db[1] == db2[1]
+    @test db[1][] === db2[1][]
+    @test db[2] == db2[2]
+    @test db[2][] === db2[2][]
+
+    # Test filtering logic
+    db2 = db[a = 1]
+    @test isa(db2, ExperimentsDB.Database)
+    @test length(db2) == 2
+    keycheck(db2)
+    @test all(isequal(1), db2[:a])
+
+    db2 = db[abba = nothing]
+    @test isa(db2, ExperimentsDB.Database)
+    @test length(db2) == 0
+
+    # Finally, test the "in" and "subset" logic
+    @test in(ExperimentsDB.todict((a = 1, b = nothing, c = 2)), db)
+    @test !in(ExperimentsDB.todict((a = 1, c = 2)), db)
+    @test issubset(ExperimentsDB.todict((a = 1, c = 2)), db)
+    @test !issubset(ExperimentsDB.todict((a = 1, d = 2)), db)
+end
+
